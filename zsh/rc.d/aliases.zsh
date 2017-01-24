d7907675181d6deece5a761849e0d03682ea147a
David Ruiz <david.ruiz@wuaki.tv>
2017-01-24 20:09:30 +0100

    More aliases

--- zsh/rc.d/aliases.zsh
+++ zsh/rc.d/aliases.zsh
@@ -15,25 +15,37 @@ alias projects='~/projects/'
 # -------------------------------------------------------------------
 # Unix
 # -------------------------------------------------------------------
+# ls, the common ones I use a lot shortened for rapid fire usage
+alias l='ls -lFh'     #size,show type,human readable
+alias la='ls -lAFh'   #long list,show almost all,show type,human readable
+alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
+alias lt='ls -ltFh'   #long list,sorted by date,show type,human readable
+alias ll='ls -l'      #long list
+alias ldot='ls -ld .*'
+alias lS='ls -1FSsh'
+alias lart='ls -1Fcart'
+alias lrt='ls -1Fcrt'
+alias dud='du -d 1 -h'
+alias duf='du -sh *'
+alias fd='find . -type d -name'
+alias ff='find . -type f -name'
 alias c='clear'
 alias k='kill -9'
 alias tlf="tail -f"
 alias ln='ln -v'
 alias mkdir='mkdir -p'
-alias ...='../..'
 alias l='ls --color'
-alias ll='ls -la --color'
-alias lh='ls -Alh --color'
 alias -g G='| grep'
 alias -g M='| less'
 alias -g L='| wc -l'
 alias -g ONE="| awk '{ print \$1}'"
-alias e="$EDITOR"
-alias v="$VISUAL"
-alias aliases="\"\$EDITOR\" ~/.aliases"
+alias grep='grep --color'
+alias sgrep='grep -R -n -H -C 5 --exclude-dir={.git,.svn,CVS} '
+alias aliases='EDITOR\" ~/.aliases'
 alias hosts='sudo \"\$EDITOR\" /etc/hosts'
 alias paux='ps aux | grep'
 alias chromium-unsecure="chromium-browser --disable-web-security"
+alias zshrc='$EDITOR ~/.zshrc'
 alias update-zsh='source ~/.zshrc'
 
 # -------------------------------------------------------------------
@@ -67,7 +79,6 @@ alias rake='noglob rake'
 alias rs='rails server'
 alias rc='rails console'
 alias rg='rails generate'
-alias z='zeus'
 alias routes='rake routes | grep'
 alias migrate='rake db:migrate'
 alias rollback='rake db:rollback'
@@ -86,6 +97,9 @@ alias psqlrestart='sudo service postgresql restart'
 # -------------------------------------------------------------------
 # Fix the swapped keys for the mac keyboard on Ubuntu
 alias fix-keyboard='echo 0 | sudo tee /sys/module/hid_apple/parameters/iso_layout'
+
+# Make zsh know about hosts already accessed by SSH
+zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
 
 # Overwrite dotfiles
 alias overwrite-dotfiles='cd $HOME/.dotfiles; make clean; make'
