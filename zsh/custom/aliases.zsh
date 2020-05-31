a84ba6f10365aabf9b7e53b99e16d557e0e5f176
David Ruiz <david.ruiz@wuaki.tv>
2020-05-31 08:00:27 +0200

    Better 'git diff' usign FZF

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -7,3 +7,8 @@ alias update-zsh='source ~/.zshrc'
 alias gitka='gitk --all'
 
 alias overwrite-dotfiles='cd $HOME/.dotfiles; make clean; make'
+
+gdiff() {
+  preview="git diff $@ --color=always -- {-1}"
+  git diff $@ --name-only | fzf -m --ansi --preview $preview
+}
