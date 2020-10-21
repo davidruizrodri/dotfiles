8ecfbb852c76b3c4f67be8b51f8143372273cfe0
David Ruiz <david.ruiz@wuaki.tv>
2020-10-21 09:29:39 +0200

    Add .gitattributes

--- git/git.mk
+++ git/git.mk
@@ -4,11 +4,13 @@ ifdef GIT
 	INSTALLERS += git
 	CLEANERS   += clean_git
 
-	GIT_SRC_DIR   := $(DOTFILES)/git
-	GITCONFIG_SRC := $(GIT_SRC_DIR)/gitconfig
-	GITCONFIG     := $(DST_DIR)/.gitconfig
-	GITIGNORE_SRC := $(GIT_SRC_DIR)/gitignore
-	GITIGNORE     := $(DST_DIR)/.gitignore
+	GIT_SRC_DIR       := $(DOTFILES)/git
+	GITCONFIG_SRC     := $(GIT_SRC_DIR)/gitconfig
+	GITCONFIG         := $(DST_DIR)/.gitconfig
+	GITIGNORE_SRC     := $(GIT_SRC_DIR)/gitignore
+	GITIGNORE         := $(DST_DIR)/.gitignore
+	GITATTRIBUTES_SRC := $(GIT_SRC_DIR)/gitattributes
+	GITATTRIBUTES     := $(DST_DIR)/.gitattributes
 
   .PHONY: git clean_git
 
@@ -16,11 +18,14 @@ ifdef GIT
 		$(LINK) $(GITCONFIG_SRC) $@
   $(GITIGNORE):
 		$(LINK) $(GITIGNORE_SRC) $@
+  $(GITATTRIBUTES):
+		$(LINK) $(GITATTRIBUTES_SRC) $@
 
-  git: dotfile_install_git $(GITCONFIG) $(GITIGNORE)
+  git: dotfile_install_git $(GITCONFIG) $(GITIGNORE) $(GITATTRIBUTES)
   clean_git: dotfile_clean_git
 		$(RM) $(GITCONFIG)
 		$(RM) $(GITIGNORE)
+		$(RM) $(GITATTRIBUTES)
 else
   @echo "Git is not installed"
 endif
