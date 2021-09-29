82de7eb7005039953959af5b314efaf23db3b917
David Ruiz <david.ruiz@wuaki.tv>
2021-09-29 10:21:07 +0200

    Change iterm2 config

--- iterm/iterm.mk
+++ iterm/iterm.mk
@@ -2,12 +2,12 @@ INSTALLERS += iterm
 CLEANERS   += clean_iterm
 
 ITERM_SRC_DIR   := $(DOTFILES)/iterm
-ITERM_CONFIG_DIR := $(CONFIG_DIR)/iterm
+ITERM_CONFIG_DIR := $(CONFIG_DIR)/iterm2
 
 .PHONY: iterm clean_iterm
 
 iterm: dotfile_install_iterm
-	$(MKDIR) $(CONFIG_DIR)/iterm
+	$(MKDIR) $(CONFIG_DIR)/iterm2
 	$(COPY) $(ITERM_SRC_DIR)/Snazzy.itermcolors $(ITERM_CONFIG_DIR)
 clean_iterm: dotfile_clean_iterm
 	$(RM) $(ITERM_CONFIG_DIR)
