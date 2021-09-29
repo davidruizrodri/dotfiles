60f8e9af1156373099dfc58b0059961c43e135a8
David Ruiz <david.ruiz@wuaki.tv>
2021-09-29 10:21:24 +0200

    Fix tmux TPM plugins installation

--- tmux/tmux.mk
+++ tmux/tmux.mk
@@ -4,22 +4,19 @@ ifdef TMUX
 	INSTALLERS += tmux
 	CLEANERS   += clean_tmux
 
-	TMUX_CONFIG_SRC  := $(DOTFILES)/tmux/tmux.conf
-	TMUX_CONFIG      := $(DST_DIR)/.tmux.conf
-	TMUX_PLUGINS_SRC := $(DOTFILES)/tmux/plugins/tpm
-	TMUX_PLUGINS     := $(DST_DIR)/.tmux/plugins/tpm
+	TMUX_CONFIG_SRC   := $(DOTFILES)/tmux/tmux.conf
+	TMUX_CONFIG       := $(DST_DIR)/.tmux.conf
+	TMUX_PLUGINS_DIR  := $(DST_DIR)/.tmux/plugins/tpm
+	TMUX_PLUGINS_REPO := https://github.com/tmux-plugins/tpm
 
   .PHONY: tmux clean_tmux
 
-  $(TMUX_CONFIG):
-		$(LINK) $(TMUX_CONFIG_SRC) $@
-  $(TMUX_PLUGINS):
-		$(LINK) $(TMUX_PLUGINS_SRC) $@
-
-  tmux: dotfile_install_tmux $(TMUX_CONFIG) $(TMUX_PLUGINS)
+  tmux: dotfile_install_tmux 
+		$(LINK) $(TMUX_CONFIG_SRC) $(TMUX_CONFIG)
+		$(CLONE) $(TMUX_PLUGINS_REPO) $(TMUX_PLUGINS_DIR)
   clean_tmux: dotfile_clean_tmux
 		$(RM) $(TMUX_CONFIG)
-		$(RM) $(TMUX_PLUGINS)
+		$(RM) $(TMUX_PLUGINS_DIR)
 else
   @echo "Tmux is not installed"
 endif
